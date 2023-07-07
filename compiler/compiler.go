266f8baafed970a2fa22d3192ca5c24bea187b87
Tyler O'Briant <tylerobriant@gmail.com>
2023-07-07 09:40:06 -0500

    starting to add support for identifiers

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -10,6 +10,7 @@ import (
 type Compiler struct {
 	instructions code.Instructions // Instructions are code.OpCodes accompanied by the location of cosntants they operate on
 	constants    []object.Object   // Store constant values in a separate location "in memory"
+	symbolTable  *SymbolTable
 
 	lastInstruction     EmittedInstruction
 	previousInstruction EmittedInstruction
@@ -24,6 +25,7 @@ func NewCompiler() *Compiler {
 	return &Compiler{
 		instructions:        code.Instructions{},
 		constants:           []object.Object{},
+		symbolTable:         NewSymbolTable(),
 		lastInstruction:     EmittedInstruction{},
 		previousInstruction: EmittedInstruction{},
 	}
@@ -71,6 +73,15 @@ func (c *Compiler) Compile(node ast.Node) error {
 		}
 
 		c.emit(code.OpPop)
+
+	case *ast.LetStatement:
+		err := c.Compile(node.Value)
+		if err != nil {
+			return err
+		}
+
+		symbol := c.symbolTable.Define(node.Name.Value)
+		c.emit(code.OpSetGlobal, symbol.Index)
 
 	case *ast.InfixExpression:
 		if node.Operator == "<" {
@@ -193,6 +204,16 @@ func (c *Compiler) Compile(node ast.Node) error {
 		} else {
 			c.emit(code.OpFalse)
 		}
+
+	case *ast.Identifier:
+		symbol, ok := c.symbolTable.Resolve(node.Value)
+
+		if !ok {
+			return fmt.Errorf("undefined variable %s", node.Value)
+		}
+
+		c.emit(code.OpGetGlobal, symbol.Index)
+
 	}
 
 	return nil
